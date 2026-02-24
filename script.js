4e602d663068077cbd461e3dceae29626a43110a
MyaPapaya555 <myargillman@gmail.com>
2026-02-24 12:26:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+<script>
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
 
+  document.querySelectorAll(".grid img").forEach(img => {
+    img.addEventListener("click", () => {
+      lightbox.classList.add("active");
+      lightboxImg.src = img.src;
+    });
+  });
+
+  lightbox.addEventListener("click", e => {
+    if (e.target !== lightboxImg) {
+      lightbox.classList.remove("active");
+    }
+  });
+</script>
